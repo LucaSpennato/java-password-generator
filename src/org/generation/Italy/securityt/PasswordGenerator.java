c7d7c666d7cb19115f73bcccb4c7cd7e09ff23cc
Luca Spennato <luca.spennato@gmail.com>
2022-11-14 16:10:03 +0100

    Initial commit

---
+++ src/org/generation/Italy/securityt/PasswordGenerator.java
@@ -0,0 +1,37 @@
+package org.generation.Italy.securityt;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserisci nome: ");
+		String userName = sc.nextLine();
+		
+		System.out.print("Inserisci cognome: ");
+		String userSurname = sc.nextLine();
+		
+		System.out.print("Inserisci il tuo colore preferito: ");
+		String userColor = sc.nextLine();
+		
+		System.out.print("Giorno di nascita: ");
+		int userDay = sc.nextInt();
+		
+		System.out.print("Mese di nascita: ");
+		int userMonth = sc.nextInt();
+		
+		System.out.print("Anno di nascita: ");
+		int userYear = sc.nextInt();
+		
+		
+		String userPassword = userName + "-" + userColor + "-" + (userDay + userMonth + userYear) ;
+		
+		System.out.println("La tua password:" + "\r" + userPassword);
+		
+		
+	}
+	
+}
